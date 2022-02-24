7605a59de2c7290d33cb23bcdc09b667e6020432
Rebecca Alarça <rebeccaalarca.art@gmail.com>
2022-02-24 14:48:33 +0000

    Variaveis inteiras

---
+++ TiposVarios/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int numero = 4
+long numeroDois = 8
+Console.WriteLine(numero);
+Console.WriteLine(long);
